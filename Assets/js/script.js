2ded620acb0abe7363e8a7cca350e8f4eb32eb1b
AN <99376802+alainatividad@users.noreply.github.com>
2022-04-10 23:52:19 +1000

    Initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.carousel').carousel();
+  });
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    shift: 50,
+    dist: -50
+});
+      
