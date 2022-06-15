224edfe0adf1027ab345a69524f2e9dab707bd74
AN <99376802+alainatividad@users.noreply.github.com>
2022-06-15 17:01:38 +1000

    Fixed layout and added css/js components

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,5 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-    $('.sidenav').sidenav();
-    $('.carousel').carousel();
-  });
-
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true,
-    shift: 50,
-    dist: -50
+// initialise materialize elements
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".dropdown-trigger").dropdown();
 });
-      
